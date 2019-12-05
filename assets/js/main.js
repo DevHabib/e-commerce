9d201965ad3a9d1d0eec8c9f8fd80986e935b633
Md Habibur Rahman <45137768+DevHabib@users.noreply.github.com>
2019-12-05 20:25:49 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+	// Jquery clickable functions starts
+	jQuery(document).ready(function ($) {
+		new WOW().init();
+
+		// tooltip starts
+		$(function () {
+			$('[data-toggle="tooltip"]').tooltip()
+		})
+		// tooltip ends
+	});
+
+	//Jquery clickable functions ends
+
+	// jQuery loading function starts          
+	jQuery(window).load(function () {
+	
+	});
+	// jQuery loading function ends
+}(jQuery));
+
